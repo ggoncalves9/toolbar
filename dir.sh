ca82a3bacf363abb6953df03fac54839347b0e96
Guilherme Goncalves <ggmilesi@gmail.com>
2024-11-29 12:03:46 -0300

    Create dir.sh

---
+++ dir.sh
@@ -0,0 +1,8 @@
+toolbar_project/
+├── toolbar/
+│   ├── assets/  # Imagens, ícones ou outros arquivos visuais
+│   ├── app.py   # Código principal da aplicação
+│   ├── utils.py # Funções auxiliares
+├── README.md    # Documentação do projeto
+├── requirements.txt  # Dependências do Python
+├── install.bat  # Script de instalação
